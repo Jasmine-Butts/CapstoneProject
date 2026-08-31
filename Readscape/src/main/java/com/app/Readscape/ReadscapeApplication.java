9f06ec35031ee60170e9b3cd494d7f2214f8e26c
Jasmine Butts <jdbutts@uncg.edu>
2026-08-31 19:28:24 -0400

    Spring Boot server creation, neon connection string not yet added.

---
+++ Readscape/src/main/java/com/app/Readscape/ReadscapeApplication.java
@@ -0,0 +1,13 @@
+package com.app.Readscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadscapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadscapeApplication.class, args);
+	}
+
+}
